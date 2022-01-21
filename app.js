7f34382fd3360b0a48e05f81cb73da496753de54
Parth Sharma <parthsharma@Parths-MacBook-Pro.local>
2022-01-21 15:56:16 +0530

    Added currOS

--- app.js
+++ app.js
@@ -4,3 +4,9 @@ const user = os.userInfo()
 console.log(user)
 // method returns systems uptime in seocnds
 console.log(`The system uptime ${os.uptime()}`)
+const currOS={
+    name:os.type(),
+    release: os.release(),
+    freeMem: os.freemem()
+}
+console.log(currOS.name)
